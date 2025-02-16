24f3555548a381d8f3f43078279144a2704c5a21
DP Bhatt <dp@sensus.dk>
2025-02-16 20:29:59 +0100

    Design fixed

--- scripts/data.js
+++ scripts/data.js
@@ -1,141 +1,37 @@
 export const data = [
   {
-    id: 1,
-    title: "Copenhagen to Kathmandu 2013 – Project Lunch",
-    eventdate: "August 1, 2013",
+    id: 12,
+    title: "Nepalese New Year 2081- With Sabin Rai and the Pharaoh (2024)",
+    eventdate: "April 19, 2024",
     images: [
-      "images/1/1.jpg",
-      "images/1/2.jpg",
-      "images/1/3.jpg"
+      "images/12/1.jpg",
+      "images/12/2.jpg",
+      "images/12/3.jpg",
+      "images/12/4.jpg",
+      "images/12/5.jpg",
+      "images/12/6.jpg",
+      "images/12/7.jpg"
     ],
-    venue: "Vega Copenhagen - Enghavevej 40, 1674 København",
-    artists: ["Local Danish artists", "Nepalese bands"],
+    videos: ["https://www.facebook.com/reel/1638973880261782"],
+    venue: "Kometen Hidovre - Hvidovrevej 280, 2650 Hvidovre",
+    artists: ["Sabin Rai", "The Pharaoh"],
     isFeatured: true,
-    description: "Local Danish and Nepalese bands performing in Copenhagen to Kathmandu 2013 was a historic event in launching our Copenhagen to Kathmandu event at its early stage. It was our very first initiation to lunch Copenhagen To Kathmandu where the overwhelming support and your participation motivated us to move on until today."
+    description: "It was a privilege to host an amazing band Sabin Rai and The Pharoah in Denmark and distribute among other European cities. It was a historic night, the first time in our Denmark community with a record-breaking huge audience. It was an outstanding performance where everyone experienced the love and respect of Nepali music and musicians. We were privileged for our audience support and our Band's awesomeness on the stage."
   },
   {
-    id: 2,
-    title: "Copenhagen to Kathmandu 2015 With Sabin Rai and Mukti Shakya",
-    eventdate: "August 1, 2015",
+    id: 11,
+    title: "Cobweb in Copenhagen 2024",
+    eventdate: "March 15, 2024",
     images: [
-      "images/2/1.jpg",
-      "images/2/2.jpg",
-      "images/2/3.jpg",
-      "images/2/4.jpg",
-      "images/2/5.jpg"
+      "images/11/1.jpg",
+      "images/11/2.jpg",
+      "images/11/3.jpg"
     ],
-    venue: "Kulturstationen Vanløse - Frode Jakobsens plads 4, 2720 København",
-    artists: ["Sabin Rai", "Mukti Shakya", "Oliver Williams and crew (live rap", "Prashant Baniya", "Culture dance", "DJ"],
+    videos: [],
+    venue: "Kulturhus Risbjerggaard - Hvidovrevej 241, 2650 Hvidovre",
+    artists: ["Cobweb (band)"],
     isFeatured: true,
-    description: "Copenhagen to Kathmandu 2015 was an event held to support earthquake victims in Nepal with various social entrepreneurship initiatives. The event was held with musical and cultural activities. We had an opportunity to organize a live concert with famous artists from Nepal Sabin Rai and Mukti Shakya to raise funds for earthquake victims in Nepal. It was an outstanding performance with a huge crowd and a memorable night."
-  },
-  {
-    id: 3,
-    title: "Copenhagen to Kathmandu 2017 with Axe Band Nepal",
-    eventdate: "January 5, 2017",
-    images: [
-      "images/3/1.jpg",
-      "images/3/2.jpg",
-      "images/3/3.jpg",
-      "images/3/4.jpg"
-    ],
-    venue: "BIBLIOTEKET - Rentemestervej 76, 2400, København",
-    artists: ["The Axe Band-Nepali"],
-    isFeatured: true,
-    description: "Renowned The Axe Band from Nepal performed in our concert Copenhagen to Kathmandu 2017 as event Headliner. After a long gap, Axe band returned once again to Copenhagen, Denmark where they played classic numbers and new creations and entertained the huge Nepalese crowd. Our local talents, musical bands, and singles also shared the platform of Copenhagen to Kathmandu with their rocking performance."
-  },
-  {
-    id: 4,
-    title: "Copenhagen to Kathmandu 2017 with Karma Band",
-    eventdate: "August 26, 2017",
-    images: [
-      "images/4/1.jpg",
-      "images/4/2.jpg",
-      "images/4/3.jpg",
-      "images/4/4.jpg",
-      "images/4/5.jpg"
-    ],
-    venue: "Koncertkirken Blågårds Pl. 6A, 2200 København",
-    artists: ["Karma Band"],
-    isFeatured: true,
-    description: "Karma Band from Nepal performed in our event \"Copenhagen To Kathmandu\" 2017. Karma band performed in Copenhagen, Denmark where we had a chance to attend and have an amazing musical experience with their popular classic numbers and new creations."
-  },
-  {
-    id: 5,
-    title: "Copenhagen to Kathmandu 2018 with Nepal Idol",
-    eventdate: "April 13, 2018",
-    images: [
-      "images/5/1.jpg",
-      "images/5/2.jpg",
-      "images/5/3.jpg",
-      "images/5/4.jpg",
-      "images/5/5.jpg",
-      "images/5/6.jpg"
-    ],
-    venue: "Shiny Star - Gammel Køge Landevej 117 Hall 'A', 2500, Valby",
-    artists: ["Buddha Lama", "Nishan Bhattarai", "Pratap Das", "NMF Artists", "INAS Artists"],
-    isFeatured: true,
-    description: "Copenhagen to Kathmandu 2018 with Nepal Idol Top 3 contestant Live Concert on Nepalese New Year's Eve 2018. The First Ever Nepal Idol season 1 Europe Tour Concert in our City Copenhagen was a historic evening - 1st Buddha Lama 2nd Nishan Bhattarai 3rd Pratap Das."
-  },
-  {
-    id: 6,
-    title: "Copenhagen to Kathmandu 2019 with Deepesh K Bhattrai, Sanjeev Pradhan and Sarisma Amatya",
-    eventdate: "April 13, 2019",
-    images: [
-      "images/6/1.jpg",
-      "images/6/2.jpg",
-      "images/6/3.jpg",
-      "images/6/4.jpg",
-      "images/6/5.jpg"
-    ],
-    venue: "Kulturstationen Vanløse - Frode Jacobsens Plads 4, 1. Sal, 2720, Vanløse, København, K",
-    artists: ["Deepesh K Bhattrai", "Sanjeev Pradhan", "Sarisma Amatya", "NMF Artists"],
-    isFeatured: true,
-    description: "Copenhagen to Kathmandu 2019 Nepali New Year’s Eve 2076 celebration event together with top tuned Nepalese singers and along with local Musician which makes the evening a historical and a memorial with DEEPESH K BHATTARAI, SANJEEV PRADHAN AND SARISMA AMATYA."
-  },
-  {
-    id: 7,
-    title: "Dashain and Tihar Event 2076 (2019) Event in Collaboration with NRNA, Denmark",
-    eventdate: "October 18, 2019",
-    images: [
-      "images/7/1.jpg",
-      "images/7/2.jpg",
-      "images/7/3.jpg",
-      "images/7/4.jpg",
-      "images/7/5.jpg"
-    ],
-    venue: "Kulturhuset Indre By - Charlotte Ammundsens Pl. 3, 1359 København",
-    artists: ["Sugam Pokharel", "Trishna Gurung", "Barsha Raut", "Local artists"],
-    isFeatured: true,
-    description: "Nepal Music Festival Denmark and NRNA Denmark presented the \"Dashain & Tihar 2076 and Peace & Unity\" charity event to support a needy child in Nepal \“PBCW\”. Well-renowned artists Mr. SUGAM POKHAREL, TRISHNA GURUNG, and BARSHA RAUT performed in Copenhagen with a huge crowd which was a successful event and most importantly we were proud to entertain our community on the occasion of Dashain and Tihar festival. We were also overwhelmed to support \“PBCW\” a dedicated organization to orphan and needy children in Nepal. Besides the local Artists were also performing - Live bands, singles, and Cultural dance shows."
-  },
-  {
-    id: 8,
-    title: "Summer World Jazz and Grill 2016 – A collaborative project with the Copenhagen Jazz Festival Project",
-    eventdate: "July 3, 2016",
-    images: [
-      "images/8/1.jpg",
-      "images/8/2.jpg"
-    ],
-    venue: "Union - Nørre Allé 7, 2200, København N",
-    artists: ["Kanta DAb DAb"],
-    isFeatured: true,
-    description: "Kanta dAb dAb is a sitar, bass, and percussion trio from Nepal. Its unique contemporary compositions create a new form of soulful fusion grooves where Eastern and Western melodies meet to offer the audience a rare musical journey. Kanta DAb DAb music draws its inspiration from the three musicians’ eclectic experiences blending ethnic, traditional, Nepalese classical, and various Western musical influences. The concert was arranged in collaboration with the Nepal Music Festival, Denmark, and World Jazz and Grill (Copenhagen Jazz project)."
-  },
-  {
-    id: 9,
-    title: "Jamming with Mukti Shakya 2021",
-    eventdate: "September 25, 2021",
-    images: [
-      "images/9/1.jpg",
-      "images/9/2.jpg",
-      "images/9/3.jpg",
-      "images/9/4.jpg"
-    ],
-    venue: "Union - Nørre Allé 7, 2200, København N",
-    artists: ["Mukti Shakya", "Danish Musician Team"],
-    isFeatured: true,
-    description: "Once again we had a huge opportunity to meet Mukti Shakya Dai and the Danish Musician team performing together on the NMF stage. There was a huge overwarmling crowd who enjoyed Mukti dai popular songs. It was challenging to satisfy the audience with a live performance with a Nepalese artist/singer and Danish musician playing together despite language barriers. It turned out to be an amazing performance. Once again we also prove that music never has language boundaries."
+    description: "It was an honor to present Cobweb in Copenhagen with a huge crowd. It was an outstanding rocking night one can never forget. Thanks to the band involved with us who were gems to the milestone of our ongoing musical journey."
   },
   {
     id: 10,
@@ -151,42 +47,158 @@ export const data = [
       "images/10/7.jpg",
       "images/10/8.jpg"
     ],
+    videos: [],
     venue: "Herlev Medborgerhus - Herlevgårdsvej 18-20, 2730, Herlev",
     artists: ["Kali Prasad Baskota", "Trishna Gurung"],
     isFeatured: true,
     description: "In 2022 we hosted a concert with Kali Prasad Baskota and Trishna Gurung Schengen/Europe tour collaborating with PBCW to fund raising for those needy children in Nepal likewise in 2019. It was a huge success for us in Denmark with a huge crowd enjoying the event. The whole Europe tour was a big success in 6 countries."
   },
   {
-    id: 11,
-    title: "Cobweb in Copenhagen 2024",
-    eventdate: "March 15, 2024",
+    id: 9,
+    title: "Jamming with Mukti Shakya 2021",
+    eventdate: "September 25, 2021",
     images: [
-      "images/11/1.jpg",
-      "images/11/2.jpg",
-      "images/11/3.jpg"
+      "images/9/1.jpg",
+      "images/9/2.jpg",
+      "images/9/3.jpg",
+      "images/9/4.jpg"
     ],
-    venue: "Kulturhus Risbjerggaard - Hvidovrevej 241, 2650 Hvidovre",
-    artists: ["Cobweb (band)"],
+    videos: [],
+    venue: "Union - Nørre Allé 7, 2200, København N",
+    artists: ["Mukti Shakya", "Danish Musician Team"],
     isFeatured: true,
-    description: "It was an honor to present Cobweb in Copenhagen with a huge crowd. It was an outstanding rocking night one can never forget. Thanks to the band involved with us who were gems to the milestone of our ongoing musical journey."
+    description: "Once again we had a huge opportunity to meet Mukti Shakya Dai and the Danish Musician team performing together on the NMF stage. There was a huge overwarmling crowd who enjoyed Mukti dai popular songs. It was challenging to satisfy the audience with a live performance with a Nepalese artist/singer and Danish musician playing together despite language barriers. It turned out to be an amazing performance. Once again we also prove that music never has language boundaries."
   },
   {
-    id: 12,
-    title: "Nepalese New Year 2081- With Sabin Rai and the Pharaoh (2024)",
-    eventdate: "April 19, 2024",
+    id: 7,
+    title: "Dashain and Tihar Event 2076 (2019) Event in Collaboration with NRNA, Denmark",
+    eventdate: "October 18, 2019",
     images: [
-      "images/12/1.jpg",
-      "images/12/2.jpg",
-      "images/12/3.jpg",
-      "images/12/4.jpg",
-      "images/12/5.jpg",
-      "images/12/6.jpg",
-      "images/12/7.jpg"
+      "images/7/1.jpg",
+      "images/7/2.jpg",
+      "images/7/3.jpg",
+      "images/7/4.jpg",
+      "images/7/5.jpg"
     ],
-    venue: "Kometen Hidovre - Hvidovrevej 280, 2650 Hvidovre",
-    artists: ["Sabin Rai", "The Pharaoh"],
+    videos: [],
+    venue: "Kulturhuset Indre By - Charlotte Ammundsens Pl. 3, 1359 København",
+    artists: ["Sugam Pokharel", "Trishna Gurung", "Barsha Raut", "Local artists"],
     isFeatured: true,
-    description: "It was a privilege to host an amazing band Sabin Rai and The Pharoah in Denmark and distribute among other European cities. It was a historic night, the first time in our Denmark community with a record-breaking huge audience. It was an outstanding performance where everyone experienced the love and respect of Nepali music and musicians. We were privileged for our audience support and our Band's awesomeness on the stage."
+    description: "Nepal Music Festival Denmark and NRNA Denmark presented the \"Dashain & Tihar 2076 and Peace & Unity\" charity event to support a needy child in Nepal \“PBCW\”. Well-renowned artists Mr. SUGAM POKHAREL, TRISHNA GURUNG, and BARSHA RAUT performed in Copenhagen with a huge crowd which was a successful event and most importantly we were proud to entertain our community on the occasion of Dashain and Tihar festival. We were also overwhelmed to support \“PBCW\” a dedicated organization to orphan and needy children in Nepal. Besides the local Artists were also performing - Live bands, singles, and Cultural dance shows."
+  },
+  {
+    id: 6,
+    title: "Copenhagen to Kathmandu 2019 with Deepesh K Bhattrai, Sanjeev Pradhan and Sarisma Amatya",
+    eventdate: "April 13, 2019",
+    images: [
+      "images/6/1.jpg",
+      "images/6/2.jpg",
+      "images/6/3.jpg",
+      "images/6/4.jpg",
+      "images/6/5.jpg"
+    ],
+    videos: [],
+    venue: "Kulturstationen Vanløse - Frode Jacobsens Plads 4, 1. Sal, 2720, Vanløse, København, K",
+    artists: ["Deepesh K Bhattrai", "Sanjeev Pradhan", "Sarisma Amatya", "NMF Artists"],
+    isFeatured: true,
+    description: "Copenhagen to Kathmandu 2019 Nepali New Year’s Eve 2076 celebration event together with top tuned Nepalese singers and along with local Musician which makes the evening a historical and a memorial with DEEPESH K BHATTARAI, SANJEEV PRADHAN AND SARISMA AMATYA."
+  },
+  {
+    id: 5,
+    title: "Copenhagen to Kathmandu 2018 with Nepal Idol",
+    eventdate: "April 13, 2018",
+    images: [
+      "images/5/1.jpg",
+      "images/5/2.jpg",
+      "images/5/3.jpg",
+      "images/5/4.jpg",
+      "images/5/5.jpg",
+      "images/5/6.jpg"
+    ],
+    videos: [],
+    venue: "Shiny Star - Gammel Køge Landevej 117 Hall 'A', 2500, Valby",
+    artists: ["Buddha Lama", "Nishan Bhattarai", "Pratap Das", "NMF Artists", "INAS Artists"],
+    isFeatured: true,
+    description: "Copenhagen to Kathmandu 2018 with Nepal Idol Top 3 contestant Live Concert on Nepalese New Year's Eve 2018. The First Ever Nepal Idol season 1 Europe Tour Concert in our City Copenhagen was a historic evening - 1st Buddha Lama 2nd Nishan Bhattarai 3rd Pratap Das."
+  },
+  {
+    id: 4,
+    title: "Copenhagen to Kathmandu 2017 with Karma Band",
+    eventdate: "August 26, 2017",
+    images: [
+      "images/4/1.jpg",
+      "images/4/2.jpg",
+      "images/4/3.jpg",
+      "images/4/4.jpg",
+      "images/4/5.jpg"
+    ],
+    videos: [],
+    venue: "Koncertkirken Blågårds Pl. 6A, 2200 København",
+    artists: ["Karma Band"],
+    isFeatured: true,
+    description: "Karma Band from Nepal performed in our event \"Copenhagen To Kathmandu\" 2017. Karma band performed in Copenhagen, Denmark where we had a chance to attend and have an amazing musical experience with their popular classic numbers and new creations."
+  },
+  {
+    id: 3,
+    title: "Copenhagen to Kathmandu 2017 with Axe Band Nepal",
+    eventdate: "January 5, 2017",
+    images: [
+      "images/3/1.jpg",
+      "images/3/2.jpg",
+      "images/3/3.jpg",
+      "images/3/4.jpg"
+    ],
+    videos: [],
+    venue: "BIBLIOTEKET - Rentemestervej 76, 2400, København",
+    artists: ["The Axe Band-Nepali"],
+    isFeatured: true,
+    description: "Renowned The Axe Band from Nepal performed in our concert Copenhagen to Kathmandu 2017 as event Headliner. After a long gap, Axe band returned once again to Copenhagen, Denmark where they played classic numbers and new creations and entertained the huge Nepalese crowd. Our local talents, musical bands, and singles also shared the platform of Copenhagen to Kathmandu with their rocking performance."
+  },
+  {
+    id: 8,
+    title: "Summer World Jazz and Grill 2016 – A collaborative project with the Copenhagen Jazz Festival Project",
+    eventdate: "July 3, 2016",
+    images: [
+      "images/8/1.jpg",
+      "images/8/2.jpg"
+    ],
+    videos: [],
+    venue: "Union - Nørre Allé 7, 2200, København N",
+    artists: ["Kanta DAb DAb"],
+    isFeatured: true,
+    description: "Kanta dAb dAb is a sitar, bass, and percussion trio from Nepal. Its unique contemporary compositions create a new form of soulful fusion grooves where Eastern and Western melodies meet to offer the audience a rare musical journey. Kanta DAb DAb music draws its inspiration from the three musicians’ eclectic experiences blending ethnic, traditional, Nepalese classical, and various Western musical influences. The concert was arranged in collaboration with the Nepal Music Festival, Denmark, and World Jazz and Grill (Copenhagen Jazz project)."
+  },
+  {
+    id: 2,
+    title: "Copenhagen to Kathmandu 2015 With Sabin Rai and Mukti Shakya",
+    eventdate: "August 1, 2015",
+    images: [
+      "images/2/1.jpg",
+      "images/2/2.jpg",
+      "images/2/3.jpg",
+      "images/2/4.jpg",
+      "images/2/5.jpg"
+    ],
+    videos: [],
+    venue: "Kulturstationen Vanløse - Frode Jakobsens plads 4, 2720 København",
+    artists: ["Sabin Rai", "Mukti Shakya", "Oliver Williams and crew (live rap", "Prashant Baniya", "Culture dance", "DJ"],
+    isFeatured: true,
+    description: "Copenhagen to Kathmandu 2015 was an event held to support earthquake victims in Nepal with various social entrepreneurship initiatives. The event was held with musical and cultural activities. We had an opportunity to organize a live concert with famous artists from Nepal Sabin Rai and Mukti Shakya to raise funds for earthquake victims in Nepal. It was an outstanding performance with a huge crowd and a memorable night."
+  },
+  {
+    id: 1,
+    title: "Copenhagen to Kathmandu 2013 – Project Lunch",
+    eventdate: "August 1, 2013",
+    images: [
+      "images/1/1.jpg",
+      "images/1/2.jpg",
+      "images/1/3.jpg"
+    ],
+    videos: [],
+    venue: "Vega Copenhagen - Enghavevej 40, 1674 København",
+    artists: ["Local Danish artists", "Nepalese bands"],
+    isFeatured: true,
+    description: "Local Danish and Nepalese bands performing in Copenhagen to Kathmandu 2013 was a historic event in launching our Copenhagen to Kathmandu event at its early stage. It was our very first initiation to lunch Copenhagen To Kathmandu where the overwhelming support and your participation motivated us to move on until today."
   },
   {
     id: 13,
@@ -205,6 +217,7 @@ export const data = [
       "images/13/10.jpg",
       "images/13/11.jpg"
     ],
+    videos: [],
     venue: "Pool Pub and Valbykulturhus",
     artists: ["Local artists"],
     isFeatured: false,
--- scripts/eventdetails.js
+++ scripts/eventdetails.js
@@ -1,73 +1,88 @@
 import { data } from './data.js';
 
-let images = [
-  "https://images.placeholders.dev/?width=600&height=600",
-  "https://images.placeholders.dev/?width=400&height=400",
-  "https://images.placeholders.dev/?width=450&height=450",
-];
-
+let images = [];
 let currentIndex = 0;
 
-function showImage(index)
-{
+function showImage(index) {
   document.getElementById("eventImage").src = images[index];
 }
 
-export function prevImage()
-{
-  currentIndex =
-    currentIndex === 0 ? images.length - 1 : currentIndex - 1;
+export function prevImage() {
+  currentIndex = currentIndex === 0 ? images.length - 1 : currentIndex - 1;
   showImage(currentIndex);
 }
 
-export function nextImage()
-{
-  currentIndex =
-    currentIndex === images.length - 1 ? 0 : currentIndex + 1;
+export function nextImage() {
+  currentIndex = currentIndex === images.length - 1 ? 0 : currentIndex + 1;
   showImage(currentIndex);
 }
 
-document.addEventListener("keydown", function (event)
-{
-  if (event.key === "ArrowLeft")
-  {
+// Keyboard navigation for image gallery
+document.addEventListener("keydown", function (event) {
+  if (event.key === "ArrowLeft") {
     prevImage();
-  } else if (event.key === "ArrowRight")
-  {
+  } else if (event.key === "ArrowRight") {
     nextImage();
   }
 });
 
-function getQueryParam(param)
-{
+function getQueryParam(param) {
   const urlParams = new URLSearchParams(window.location.search);
   return urlParams.get(param);
 }
 
-function loadEventDetails()
-{
+function loadEventDetails() {
   const eventId = getQueryParam("id");
-  if (!eventId)
-  {
+  if (!eventId) {
     document.getElementById("eventTitle").textContent = "Event Not Found";
     return;
   }
 
   const event = data.find(e => e.id === parseInt(eventId));
 
-  if (event)
-  {
+  if (event) {
     document.getElementById("eventTitle").textContent = event.title;
     document.getElementById("eventDate").textContent = event.eventdate;
     document.getElementById("eventLocation").textContent = event.venue;
     document.getElementById("eventArtists").textContent = event.artists.join(", ");
     document.getElementById("eventDetail").textContent = event.description;
-    document.getElementById("eventImage").src = event.images[0];
-    images = event.images;
+    
+    // Load event images
+    if (event.images && event.images.length > 0) {
+      images = event.images;
+      document.getElementById("eventImage").src = images[0];
+      document.getElementById("eventImage").alt = event.title;
+    } else {
+      document.getElementById("eventImage").src = "https://via.placeholder.com/500x300?text=No+Image";
+    }
 
-    document.getElementById("eventImage").alt = event.title;
-  } else
-  {
+    // Load event videos
+    const videoContainer = document.getElementById("eventVideoContainer");
+    videoContainer.innerHTML = ""; // Clear previous content
+
+    if (event.videos && event.videos.length > 0) {
+      videoContainer.style.display = "block"; // Show video section
+
+      event.videos.forEach(videoUrl => {
+        const iframe = document.createElement("iframe");
+        iframe.src = videoUrl.replace("facebook.com/reel/", "facebook.com/plugins/video.php?href=https://www.facebook.com/reel/");
+        iframe.width = "68%";
+        iframe.height = "350";
+        iframe.style.border = "none";
+        iframe.style.overflow = "hidden";
+        iframe.allow = "autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share";
+        iframe.allowFullscreen = true;
+
+        const videoWrapper = document.createElement("div");
+        videoWrapper.classList.add("video-wrapper");
+        videoWrapper.appendChild(iframe);
+
+        videoContainer.appendChild(videoWrapper);
+      });
+    } else {
+      videoContainer.style.display = "none"; // Hide if no videos available
+    }
+  } else {
     document.getElementById("eventTitle").textContent = "Event Not Found!";
   }
 }
